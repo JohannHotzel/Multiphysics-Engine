60ce2f4c003f63ae24ec7e31a255aa1bf2e8a1ab
Tomaso <66296094+johanntomaso@users.noreply.github.com>
2025-07-10 00:23:37 +0200

    cloth x sphere collider collision handling 🧵⚪️💥

--- CollisionDetector.cs
+++ CollisionDetector.cs
@@ -10,17 +10,56 @@ public static class CollisionDetector
         if (!p.solveForCollision) return null;
 
         Collider[] hits = Physics.OverlapSphere(predictedPos, p.radius * 1.1f);
+
         if (hits.Length == 0) return null;
-        MeshCollider meshCollider = hits.FirstOrDefault(h => h is MeshCollider) as MeshCollider;
-        if (meshCollider == null) return null;
+        Collider col = hits[0];
 
+        Vector3 point;
+        Vector3 normal;
+        Rigidbody rb = col.attachedRigidbody;
 
-        ClosestPoint cp = ClosestPointOnMesh.GetClosestPointOnMeshNormal(meshCollider, predictedPos);
-        if (cp == null) return null;
+        //------- MeshCollider ------------------------------------------------------------------------------
+        if (col is MeshCollider meshCol)
+        {
+            var cp = ClosestPointOnMesh.GetClosestPointOnMeshNormal(meshCol, predictedPos);
+            if (cp == null) return null;
 
-        Rigidbody rb = meshCollider.attachedRigidbody;
+            point = cp.point;
+            normal = cp.normal;
+        }
 
-        return new CollisionConstraint(p, cp.point, cp.normal, p.radius, solver, rb);
+        //------- SphereCollider ------------------------------------------------------------------------------
+        else if (col is SphereCollider sphereCol)
+        {
+            Vector3 center = sphereCol.transform.TransformPoint(sphereCol.center);
+
+            float uniformScale = sphereCol.transform.lossyScale.x;
+            float worldRadius = sphereCol.radius * uniformScale;
+
+            Vector3 dir = predictedPos - center;
+            float dist = dir.magnitude;
+
+            if (dist > worldRadius + p.radius)
+                return null;
+
+            normal = dir.normalized;
+            point = center + normal * worldRadius;
+        }
+
+        //------- CapsuleCollider ------------------------------------------------------------------------------
+        else if (col is CapsuleCollider capCol)
+        {
+            point = Vector3.zero;
+            normal = Vector3.up;
+        }
+
+        //------- Else -----------------------------------------------------------------------------------------
+        else
+        {
+            return null;
+        }
+
+        return new CollisionConstraint(p, point, normal, p.radius, solver, rb);
     }
 }
 
