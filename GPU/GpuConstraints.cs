c6d4362733480c4340eac5dbb569a6dcf641ca2c
Tomaso <66296094+johanntomaso@users.noreply.github.com>
2025-10-07 23:08:23 +0200

    Rearrange Folders

---
+++ GPU/GpuConstraints.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public struct GpuDistanceConstraint
+{
+    public uint i, j;
+    public float rest;
+    public float compliance;
+
+    public const int Stride = sizeof(uint) * 2 + sizeof(float) * 2; // float*2 + float3*2 = 20 bytes
+
+    public GpuDistanceConstraint(uint a, uint b, float r, float c)
+    {
+        i = a;
+        j = b;
+        rest = r;
+        compliance = c;
+    }
+}
+
+public struct GpuCollisionConstraint
+{
+    public Vector3 target;
+    public Vector3 normal;
+    public float radius;
+
+    public const int Stride = (3 + 3 + 1) * sizeof(float); // 28
+}
